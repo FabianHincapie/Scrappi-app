b0756b02d426fd613e9e75e5c9826a3661dffa61
Jmendoti <jmendoti21@gmail.com>
2026-02-24 12:23:56 -0500

    feat: estructura base de Angular con NG-Bootstrap y Backend con Spring Boot

---
+++ backend/scrappi/src/main/java/com/talentotech/scrappi/ScrappiApplication.java
@@ -0,0 +1,13 @@
+package com.talentotech.scrappi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrappiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScrappiApplication.class, args);
+	}
+
+}
